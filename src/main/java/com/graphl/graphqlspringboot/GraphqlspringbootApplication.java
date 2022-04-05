9a13f16989bb095834c98c711396abdf9735edb7
Lewis <lewisthornton@btinternet.com>
2022-04-05 17:05:48 +0100

    Initial Dump

---
+++ src/main/java/com/graphl/graphqlspringboot/GraphqlspringbootApplication.java
@@ -0,0 +1,15 @@
+package com.graphl.graphqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlspringbootApplication.class, args);
+		// Click me for the tutorial this project is following
+		// https://medium.com/swlh/how-to-write-a-simple-graphql-application-using-spring-boot-in-java-a8232a0decd5
+	}
+
+}
---
+++ src/test/java/com/graphl/graphqlspringboot/GraphqlspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphl.graphqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
